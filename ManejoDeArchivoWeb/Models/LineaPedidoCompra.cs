fbcbbb241e0b32ef487c1a9005a41d3d4fb1fb94
Jose Jimenez <josejimenez@MacBook-Pro-de-Jose.local>
2022-05-20 19:34:12 -0600

    FIRST COMMIT

---
+++ ManejoDeArchivoWeb/Models/LineaPedidoCompra.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ManejoDeArchivoWeb.Models;
+
+	public class LineaPedidoCompra
+	{
+    public int CodigoProducto { get; set; }
+    public string? Nombre { get; set; }
+    public string? Almacen { get; set; }
+    public string? Color { get; set; }
+    public string? Talla { get; set; }
+    public string? Estilo { get; set; }
+    public decimal CantidadCompra { get; set; }
+    public string? UnidadCompra { get; set; }
+}
+
---
+++ ManejoDeArchivoWeb/Models/PedidoCompra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ManejoDeArchivoWeb.Models;
+
+	public class PedidoCompra
+	{
+    public string? CodigoCompra { get; set; }
+    public string? NombreProveedor { get; set; }
+    public DateTime FechaCompra { get; set; }
+    public List<LineaPedidoCompra>? Lineas { get; set; }
+
+    }
+
