4927bba611117f0ae4c5bcbcbead71f43d04ad92
Jose Jimenez <josejimenez@MacBook-Pro-de-Jose.local>
2022-05-20 19:34:34 -0600

    second commit

---
+++ ManejoDeArchivoWeb/Models/LineasPedidoVenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ManejoDeArchivoWeb.Models;
+
+	public class LineasPedidoVenta
+	{
+    public int CodigoProducto { get; set; }
+    public string? Nombre { get; set; }
+    public string? Almacen { get; set; }
+    public string? Color { get; set; }
+    public string? Talla { get; set; }
+    public string? Estilo { get; set; }
+    public decimal CantidadVenta { get; set; }
+    public string? UnidadVenta { get; set; }
+    }
+
---
+++ ManejoDeArchivoWeb/Models/PedidoVenta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ManejoDeArchivoWeb.Models;
+
+	public class PedidoVenta
+	{
+		public string? CodigoVenta { get; set; }
+		public string? NombreCliente { get; set; }
+		public DateTime FechaVenta { get; set; }
+		public List<LineasPedidoVenta>? Lineas { get; set; }
+	}
+
